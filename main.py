fe771a107d9d91e2976c8f77fac9913c6bbf5fb3
Lyndsey Padget <lyndsey.padget@vmlyr.com>
2021-01-24 18:43:13 -0600

    Read configuration from an ini file instead of prompting the user

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import getpass
+import configparser
+import os.path
+from os import path
 from collections import namedtuple
 from lxml import objectify
 from project import Project
 from importer import Importer
 from labelcolourselector import LabelColourSelector
-
 
 def read_xml_sourcefile(file_names):
     files = list()
@@ -15,23 +17,29 @@ def read_xml_sourcefile(file_names):
     return files
 
 
-file_names = input(
-    'Path to JIRA XML query file (semi-colon separate for multiple files): ')
-all_xml_files = read_xml_sourcefile(file_names)
+config = configparser.ConfigParser()
+if not path.exists('config.ini'):
+    print('ERROR: No config.ini found in this directory')
+    exit()
 
-jira_proj = input('JIRA project name to use: ')
-jira_done_id = input('JIRA Done issue status ID: ')
-us = input('GitHub account name (user or organization): ')
-repo = input('GitHub target repository name: ')
-user = input('GitHub username: ')
-pw = input('GitHub password: ')
-start_from_issue = input('Start from (0 = beginning): ')
+config.read('config.ini')
+
+jira_project = config['Settings']['jira_project']
+jira_done_id = config['Settings']['jira_done_id']
+user_or_org = config['Settings']['user_or_org']
+repo_name = config['Settings']['repo_name']
+username = config['Settings']['username']
+password = config['Settings']['password']
+start_from_issue = config['Settings']['start_from_issue']
 start_from_issue = 0 if (start_from_issue=='') else start_from_issue
 
-Options = namedtuple("Options", "user passwd account repo")
-opts = Options(user=user, passwd=pw, account=us, repo=repo)
+file_names = config['Settings']['file_names']
+all_xml_files = read_xml_sourcefile(file_names)
 
-project = Project(jira_proj, jira_done_id)
+Options = namedtuple("Options", "user passwd account repo")
+opts = Options(user=username, passwd=password, account=user_or_org, repo=repo_name)
+
+project = Project(jira_project, jira_done_id)
 
 for f in all_xml_files:
     for item in f.channel.item:
